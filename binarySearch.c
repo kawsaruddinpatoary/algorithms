8de537443fcd7fcb1e219eec8b067918a680dec9
kawsar <kawsaruddin350@gmail.com>
2025-05-31 12:56:00 +0600

    recursive binary search

--- binarySearch.c
+++ binarySearch.c
@@ -18,9 +18,25 @@ int iterativeBinarySearch(int A[], int n, int key){
     return -1;
 }
 
+int recursiveBinarySearch(int A[], int l, int h, int key){
+    if(l>h){
+        return -1;
+    }
+    int mid=(l+h)/2;
+    if(A[mid]==key){
+        return mid;
+    }
+    if(A[mid]>key){
+        return recursiveBinarySearch(A, l, mid-1, key);
+    }else{
+        return recursiveBinarySearch(A, mid+1, h, key);
+    }
+}
+
+
 int main(){
     int A[] = {10, 20, 30, 35, 40, 45, 60, 65, 70, 75, 80};
-    int index = iterativeBinarySearch(A, 11, 10);
+    int index = recursiveBinarySearch(A, 1, 10, 75);
     index>=0 ? printf("Found at index %d.\n", index) : printf("Not found.\n");
     return 0;
 }
